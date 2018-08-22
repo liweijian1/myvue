75050a0740226530d0ba3a3f19e98402fdf69eac
liweijian <928719621@qq.com>
2018-08-22 11:08:14 +0800

    test1

--- src/base/lwj-dialog.vue
+++ src/base/lwj-dialog.vue
@@ -1,6 +1,13 @@
 <template>
+  <!--对话框，transition用于过渡-->
   <transition name="lwj-dialog-fade">
-     <div v-show="openDialog"></div>
+     <div class="lwj-dialog-box" v-show="dialogShow">
+       <div class="lwj-dialog-wrapper">
+         <div class="lwj-dialog-content">
+           <div class="lwj-dialog-head" v-text="headText"></div>
+         </div>
+       </div>
+     </div>
   </transition>
 </template>
 <script>
