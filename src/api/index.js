efdc550b27b3be4ec1fedad04b49f3ddbc67a926
liweijian <928719621@qq.com>
2018-09-11 16:09:02 +0800

    2018/9/11第一次提交

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line
 /* eslint-disable */
-import {URL} from 'assets/js/config.js'
+import {URL} from '../assets/js/config.js'
+import axios from 'axios'
 
+// 排行榜列表
+export function getToplistDetail() {
+  const url = '/top/list';
+  return axios.get(url)
+}
--- src/pages/topList/topList.vue
+++ src/pages/topList/topList.vue
@@ -3,4 +3,29 @@
     云音乐特色榜
   </div>
 </template>
-<script></script>
+<script>// eslint-disable-next-line
+  /* eslint-disable */
+  import {getToplistDetail} from './../../api/index'
+
+  export default {
+    name:'topList',
+
+    data(){
+      return{
+        list:[],
+      }
+    },
+
+    created(){
+      //获取音乐排行榜
+      const _getToplistDetail = getToplistDetail().then((res) =>{
+         if(res.data.code===200){
+           let list;
+           console.log(res.data);
+         }
+      });
+      Promise.all([_getToplistDetail]);
+    }
+  }
+
+</script>
